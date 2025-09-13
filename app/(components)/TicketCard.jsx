5c3d4d12783eaa94e585e5994fb38cfb2f81dc08
andresvillay <andresvillay2@gmail.com>
2025-09-13 17:43:40 +0200

    updated ticket card with categories from backend

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -10,20 +10,20 @@ const TicketCard = ({ticket}) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
-        <PriorityDisplay priority={ticket.priority} />
+        <PriorityDisplay priority={ticket.prioi} />
         <div className="ml-auto">
           <DeleteButton />
         </div>
       </div>
-      <h4>Ticket Title</h4>
+      <h4>{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
       <p className="whitespace-pre-wrap">
-        This is the ticket description. Please work on this ticket.
+        {ticket.description}
       </p>
       <div className="flex-grow"></div>
       <div className="flex mt-2 justify-between items-center">
         <div className="flex flex-col">
-          <p className="text-xs">31.08.2023 - 10:43 AM</p>
+          <p className="text-xs">{ticket.createdAt}</p>
           <ProgressDisplay />
         </div>
         <div>
